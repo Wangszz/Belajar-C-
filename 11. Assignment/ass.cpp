74508416b07e75657a85f98dd3cd81bbcace34e0
Wangszz <suryadewangga09@gmail.com>
2022-05-13 00:17:52 +0700

    dkdk

--- 11. Assignment/ass.cpp
+++ 11. Assignment/ass.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
 
     int a = 10;
+    int b = 10;
+    int c = 10;
 
     cout << "Nilai awal adalah= " << a << endl;
 
@@ -19,6 +21,20 @@ int main() {
     cout << "setelah menggunakan assignment operator menjadi= " << a << endl;
     a %= 3;
     cout << "setelah menggunakan assignment operator menjadi= " << a << endl;
+
+    //post dan preincrement
+
+    //post increment
+    //b++ sama saja dengan a = a + 1 (pwrhitungan akan muncul diakhir)
+    cout << b << endl;
+    cout << b++ << endl;
+    cout << b << endl << endl;
+
+    //prestincrement ++c (perhitungan akan langsung muncul)
+    cout << c << endl;
+    cout << ++c << endl;
+    cout << c << endl;
+
 
     cin.get();
     return 0;
