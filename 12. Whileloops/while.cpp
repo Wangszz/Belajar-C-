af2b58348508d77d2a14579f338d38563b14e3ab
Wangszz <suryadewangga09@gmail.com>
2022-05-13 21:12:34 +0700

    adding whileloops

---
+++ 12. Whileloops/while.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 15;
+
+    // while(syarat){
+    //     aksi
+    // }
+
+    while(a <= 20){
+        cout << "asik";
+        cout << a << endl;
+        a += 1; //untuk membatasi a, jadi jika lebih dari 20 maka akan berhenti
+    }
+
+
+    cin.get();
+    return 0;
+}
